9018da36e74e8836aa4fe48d329c0aff18f99794
Ammar Ansari <iammaransari@gmail.com>
2022-06-26 23:47:07 +0500

    material ui setup with theme config

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
 
+import theme from './theme';
 import './App.css';
 
 const Home = lazy(() => import('./routes/Home'));
 
 function App() {
   return (
-    <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </Suspense>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </ThemeProvider>
   );
 }
 

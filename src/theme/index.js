3330e6f1a2e04b52ae6c53514bbb6c3c10e1c216
Ammar Ansari <iammaransari@gmail.com>
2022-06-30 00:09:47 +0500

    app font setup with mui

--- src/theme/index.js
+++ src/theme/index.js
@@ -2,7 +2,9 @@ import { createTheme } from '@mui/material/styles';
 
 // A custom theme for this app
 const theme = createTheme({
-  palette: {},
+  typography: {
+    fontFamily: ['Jost', 'sans-serif'].join(','),
+  },
 });
 
 export default theme;

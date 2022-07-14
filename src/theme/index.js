fad7ec925ce633d53b8bff2dff41f7b49d9560f5
Ammar Ansari <iammaransari@gmail.com>
2022-07-14 21:19:25 +0500

    application font size update

--- src/theme/index.js
+++ src/theme/index.js
@@ -3,7 +3,7 @@ import { createTheme } from '@mui/material/styles';
 // A custom theme for this app
 const theme = createTheme({
   typography: {
-    fontFamily: ['Jost', 'sans-serif'].join(','),
+    fontFamily: ['Futura Lt BT', 'sans-serif'].join(','),
   },
   palette: {
     brown: '#D8A08F',
